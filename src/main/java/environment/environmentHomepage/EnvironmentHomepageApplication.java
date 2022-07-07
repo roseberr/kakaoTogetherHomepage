1a6816ceafc0ec09dc64d57fc8859e6de4533c7b
roseberr <bsc981111@gmail.com>
2022-07-07 14:49:11 +0900

    Initial commit

---
+++ src/main/java/environment/environmentHomepage/EnvironmentHomepageApplication.java
@@ -0,0 +1,13 @@
+package environment.environmentHomepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvironmentHomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvironmentHomepageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/environment/environmentHomepage/EnvironmentHomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package environment.environmentHomepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvironmentHomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
