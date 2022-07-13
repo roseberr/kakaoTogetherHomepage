51278b0e5bb333ba3895bf5de04394c8d75a4b59
roseberr <bsc981111@gmail.com>
2022-07-14 00:27:11 +0900

    commit

--- src/main/java/environment/environmentHomepage/controller/PostsApiController.java
+++ src/main/java/environment/environmentHomepage/controller/PostsApiController.java
@@ -1,9 +1,12 @@
 package environment.environmentHomepage.controller;
 
 
+import environment.environmentHomepage.domain.board.Posts;
 import environment.environmentHomepage.domain.dto.PostsSaveRequestDto;
 import environment.environmentHomepage.service.PostsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +18,17 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
+
+    @GetMapping("/api/posts")
+    public String posts(Model model){
+        model.addAttribute("",new PostsSaveRequestDto());
+        return "/post/register";
+    }
+
 // post 등록하기 controller
     @PostMapping("/api/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
     }
-
 
 }
--- src/main/java/environment/environmentHomepage/domain/board/Posts.java
+++ src/main/java/environment/environmentHomepage/domain/board/Posts.java
@@ -14,6 +14,7 @@ import javax.persistence.*;
 @Entity
 
 public class Posts extends BaseTimeEntity {
+
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long board_id;
