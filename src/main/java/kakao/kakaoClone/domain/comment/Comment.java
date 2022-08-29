0744d0768e3e4b4602d18d263729d832bfc4140b
solchan_bhang <bsc981111@naver.com>
2022-08-29 19:06:21 +0900

    comment domain 생성

---
+++ src/main/java/kakao/kakaoClone/domain/comment/Comment.java
@@ -0,0 +1,40 @@
+package kakao.kakaoClone.domain.comment;
+
+
+import kakao.kakaoClone.domain.BaseTimeEntity;
+import kakao.kakaoClone.domain.board.Post;
+import kakao.kakaoClone.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Entity
+public class Comment extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    /** 댓글 내용 수정 **/
+    public void update(String content) {
+        this.content = content;
+    }
+
+}
---
+++ src/main/java/kakao/kakaoClone/domain/comment/CommentRepository.java
@@ -0,0 +1,7 @@
+package kakao.kakaoClone.domain.comment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+}
